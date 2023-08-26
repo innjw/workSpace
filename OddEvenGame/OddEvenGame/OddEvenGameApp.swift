be511c83c046f6c13068db71cfc415b1784e0ac4
innjw <67609683+innjw@users.noreply.github.com>
2023-08-26 21:02:16 +0900

    create new project

---
+++ OddEvenGame/OddEvenGame/OddEvenGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  OddEvenGameApp.swift
+//  OddEvenGame
+//
+//  Created by 인재욱 on 2023/08/26.
+//
+
+import SwiftUI
+
+@main
+struct OddEvenGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
